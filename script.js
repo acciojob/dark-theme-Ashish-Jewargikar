0e85bd4f8eeab5687491a12269b82e0328c41b2a
AshIsh <33283957+Ashish-Jewargikar@users.noreply.github.com>
2023-04-22 16:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+const button = document.querySelector('button');
+const main = document.querySelector('main');
+
+button.addEventListener('click', function() {
+  main.classList.toggle('night');
+  button.classList.toggle('button_night');
+});
 
